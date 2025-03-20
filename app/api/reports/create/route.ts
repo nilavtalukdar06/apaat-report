c419ccd12ef004a5fa6875c8b8b0eca3a95a1520
Nilav <nilavtalukdar06@gmail.com>
2025-03-20 23:51:37 +0530

    Minor code updates

--- app/api/reports/create/route.ts
+++ app/api/reports/create/route.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { ReportType } from "@prisma/client";
-
+export const dynamic = "force-dynamic";
 export async function POST(request: NextRequest) {
   try {
     const {
