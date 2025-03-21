e29fb3a1961d656fbdd2299e1019c86b0dbf75f6
Nilav <nilavtalukdar06@gmail.com>
2025-03-21 08:35:59 +0530

    Added donate us route

---
+++ app/donateus/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function DonateUs() {
+  return (
+    <main className="min-h-screen max-w-screen relative overflow-x-hidden">
+      <section className="max-w-xl mx-auto my-8 sm:my-16">
+        <p className="text-white">Donate Us</p>
+      </section>
+    </main>
+  );
+}
